7d488982b60ebd40eb9960220cdd81e20e84dcbc
Stephano Luiz Balbinot <stephanobalbinot@hotmail.com>
2020-01-15 22:46:55 -0300

    Adicionado solução, projeto System e classe Application

---
+++ System/Application.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public class Application
+    {
+        public string OpenApplication(string fileName, string arguments, int timeout)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+                throw new ArgumentNullException("fileName");
+
+            string applicationWindow;
+
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            startInfo.FileName = fileName;
+
+            if (!string.IsNullOrWhiteSpace(arguments))
+                startInfo.Arguments = arguments;
+
+            process.StartInfo = startInfo;
+
+            process.Start();
+
+            process.WaitForInputIdle(timeout);
+
+            applicationWindow = process.MainWindowTitle;
+
+            process.Close();
+
+            return applicationWindow;
+        }
+
+        public void CloseApplication(string processName, int timeout)
+        { 
+            if (string.IsNullOrWhiteSpace(processName))
+                throw new ArgumentNullException("processName");
+
+            if (processName.Contains(".exe"))
+                processName = processName.Replace(".exe", "");
+
+            foreach (var process in Process.GetProcessesByName(processName))
+            {
+                process.CloseMainWindow();
+                process.WaitForExit(timeout);
+                if (process.HasExited)
+                    process.Close();
+                else
+                    throw new InvalidOperationException("An unknown error occurred. The process '" + processName + "' failed to exit.");
+            }
+        }
+
+        public void StartProcess(string fileName, string arguments)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+                throw new ArgumentNullException("fileName");
+
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            startInfo.FileName = fileName;
+
+            if (!string.IsNullOrWhiteSpace(arguments))
+                startInfo.Arguments = arguments;
+
+            process.StartInfo = startInfo;
+
+            process.Start();
+
+            process.Close();
+        }
+
+        public void KillProcess(string processName)
+        {
+            if (string.IsNullOrWhiteSpace(processName))
+                throw new ArgumentNullException("processName");
+
+            if (processName.Contains(".exe"))
+                processName = processName.Replace(".exe", "");
+
+            foreach (var process in Process.GetProcessesByName(processName))
+            {
+                process.CloseMainWindow();
+                process.Kill();
+                process.Close();
+            }
+        }
+    }
+
+    
+}
---
+++ System/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System
+{
+    class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
