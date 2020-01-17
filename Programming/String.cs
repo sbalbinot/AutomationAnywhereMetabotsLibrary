7f0d55cb34d85d814dabd8644b7a0fa1fc3159b8
Stephano Luiz Balbinot <stephanobalbinot@hotmail.com>
2020-01-17 16:27:29 -0300

    Adicionado projeto Programming -> Classe String

---
+++ Programming/String.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming
+{
+    public class String
+    {
+        public string AddDelimiterBetweenStringCharacters (string text, string delimiter)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                throw new ArgumentNullException("text");
+
+            if (string.IsNullOrEmpty(delimiter))
+                throw new ArgumentNullException("delimiter");
+
+            return string.Join(delimiter, text.Select(c => c.ToString()).ToArray());
+        }
+    }
+}
