6a8b7346faeaca71eebbb4a699fbf2425eda6f35
slbalbinot <stephanobalbinot@hotmail.com>
2020-03-25 18:54:13 -0300

    Adicionada classe Number -> método GetDifference

---
+++ Programming/Number.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace Programming
+{
+    public class Number
+    {
+        public string GetDifference(double number, double number2, string format)
+        {
+            double difference = number - number2;
+
+            return System.String.Format(CultureInfo.CurrentCulture, format, difference);
+        }
+    }
+}
